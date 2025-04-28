65963cff2afb267c240ddb0d32abb9f242840470
Carlos Melchor Perez <carloos0665@gmail.com>
2025-04-28 22:51:26 +0000

    reltaionShip

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
@@ -9,11 +10,49 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    user_name : Mapped[str]= mapped_column(String(50), unique=True, nullable=False)
+    first_name : Mapped[str]= mapped_column(String(50), unique=True, nullable=False)
+    last_name : Mapped[str]= mapped_column(String(50), unique=True, nullable=False)
 
+
+    #One to many relationship
+    follower: Mapped[List["Follower"]] = relationship(back_populates="user")
+    follows: Mapped[List["Follows"]] = relationship(back_populates="user")
+    post: Mapped[List["Post"]] = relationship(back_populates="user")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
+
+
+class Follower(db.Model):
+    id : Mapped[int] = mapped_column(primary_key=True)
+    user_name : Mapped[str]= mapped_column(String(50), unique=True, nullable=False)
+
+    #USER RELATIONSHIP
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="follower")
+
+class Follows(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_name : Mapped[str]= mapped_column(String(50), unique=True, nullable=False)
+
+    #USER RELATIONSHIP
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="follows")
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    content : Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+
+    #USER RELATIONSHIP
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="post")
+
+class comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text : Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
+    author_id : Mapped[int] = mapped_column(primary_key=True)
+    from_post_id : Mapped[int] = mapped_column(primary_key=True)
